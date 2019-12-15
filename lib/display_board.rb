e8c83eb1580a81f4bcb2e5474529db68ce7644b4
Vadym Drozdovych <vadim.drozdovich@gmail.com>
2019-12-15 14:55:37 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,4 @@
-board = [" X "," O "," X "," O "," X "," O "," X "," O "," X "]
+board = ["  ", "  ", "  ", " O "," X "," O "," X "," O "," X "]
 def display_board(board)
   puts board[0, 1, 2]
   puts board[3, 4, 5]
